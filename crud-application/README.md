060d0e4e7e250741fafe294d4b2c8f011aac7c53
wascoyur <mytem20007@gmail.com>
2020-12-16 22:10:44 +0500

    error: просит постгрес в ноде модулс...

--- crud-application/README.md
+++ crud-application/README.md
@@ -1,2 +1,24 @@
 # source of inspiration #
 https://bezkoder.com/node-express-mongodb-crud-rest-api/
+### mysql ###
+https://bezkoder.com/node-js-express-sequelize-mysql/
+Node.js Rest CRUD API overview
+Demo Video
+Create Node.js App
+Setup Express web server
+Configure MySQL database & Sequelize
+Initialize Sequelize
+Define the Sequelize Model
+Create the Controller
+Create a new object
+Retrieve objects (with condition)
+Retrieve a single object
+Update an object
+Delete an object
+Delete all objects
+Find all objects by condition
+Define Routes
+Test the APIs
+Source code
+Conclusion
+Further Reading
--- crud-application/src/api/server.js
+++ crud-application/src/api/server.js
@@ -1,5 +1,21 @@
-// Build a server with Node's HTTP module
-const http = require('http');
-const port = 3001;
-const server = http.createServer();
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
+
+let corsOptions = {
+  origin: "http://localhost:3001"
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) =>{
+  res.json({message: 'Запрос получен'})
+});
+
+const PORT = process.env || 3001;
+app.listen(PORT, ()=>{
+  console.log(`Сервер запущен на ${PORT} порту`)
+})
