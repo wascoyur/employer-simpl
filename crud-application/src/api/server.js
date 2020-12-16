5bbeff960e519b68c70fc6c141c39801d89caac1
mku <mytem20007@gmail.com>
2020-12-16 17:23:21 +0500

    add server.js

--- crud-application/src/api/server.js
+++ crud-application/src/api/server.js
@@ -1,5 +1,20 @@
-// Build a server with Node's HTTP module
-const http = require('http');
-const port = 3001;
-const server = http.createServer();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+let corsOptions = {
+  origin: 'http://localhost:8080'
+}
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.Router().get("/",(req, res) => {
+  res.json({ message: 'Сервер старт'})
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, ()=>{
+  console.log(`Сервер запущен на порту: ${PORT}`)
+})
 
